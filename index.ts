8aea888ed0ed1063b209fbb8f80012a4ec02be3a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-19 21:26:36 +0800

    initial index

---
+++ index.ts
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app  = express();
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+];
+
+app.get('/', (_req, res) => {
+  return res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (_req, res) => {
+  return res.json(notes);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
