4a0073f4a4ea373085225db836c8979788dee42e
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-26 21:47:43 +0800

    error handler moved to middleware file

--- utils/middleware.ts
+++ utils/middleware.ts
@@ -19,3 +19,21 @@ const unknownEndpoint = (_req: Request, res: Response) => {
   });
 };
 
+// Error handler
+const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id'});
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message});
+  }
+
+  return next(error);
+};
+
+export default {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+};
