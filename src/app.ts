acbc79555d484a075b8d6f3331e569990e2b189c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-26 21:52:50 +0800

    created app file

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import config from '../utils/config';
+import express from 'express';
+import cors from 'cors';
+import notesRouter from './routes/noteRouter';
+import mongoose from 'mongoose';
+import logger from '../utils/logger';
+import middleware from '../utils/middleware';
+
+// Initialize express app
+const app  = express();
+
+// Initiate MongoDB connection
+mongoose.set('strictQuery', false);
+logger.info('connecting to', config.MONGODB_URI!);
+mongoose.connect(config.MONGODB_URI!)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error: unknown) => {
+    if (error instanceof Error) {
+      logger.error('error connecting to MongoDB:', error.message);
+    }
+  });
+
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+
+// Set up routing for notes
+app.use('/api/notes', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+export default app;
