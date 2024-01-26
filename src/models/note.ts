539c9556fc9bd39114540c869602180fab67917e
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-26 21:46:07 +0800

    note model now only contains noteSchema definition

--- src/models/note.ts
+++ src/models/note.ts
@@ -1,25 +1,12 @@
 import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
 import { Note } from '../types/notes';
-dotenv.config();
-
-mongoose.set('strictQuery', false);
-
-if (process.env.MONGODB_URI) {
-  const url = process.env.MONGODB_URI;
-  mongoose.connect(url)
-    .then(() => console.log('connected to MongoDB'))
-    .catch((error: unknown) => {
-      let errorMessage = 'Error connecting to MongoDB!';
-      if (error instanceof Error) {
-        errorMessage += ` Error: ${error.message}`;
-      }
-      console.log(errorMessage);
-    });
-}
 
 const noteSchema = new mongoose.Schema<Note>({
-  content: String,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
   important: Boolean
 });
 
