379808a74fcb20d578b489f9fd6f64db8de0a902
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-26 21:51:13 +0800

    implement GET specific note given id

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -8,3 +8,12 @@ const getAllNotes =  (_req: Request, res: Response, next: NextFunction) => {
     .then(notes => res.json(notes))
     .catch(error => next(error) );
 };
+
+// GET a specific note given id
+const getNote = (req: Request, res: Response, next: NextFunction) => {
+  NoteModel.findById(req.params.id)
+    .then(note => {
+      note ? res.json(note) : res.status(404).json({ error: 'Cannot find note with that id'}).end()
+    })
+    .catch(error => next(error));
+};
