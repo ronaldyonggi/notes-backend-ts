830294356da8763f1f08f1781272079efca6db7f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-07 15:38:47 +0800

    fix toNewNote import source

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import NoteModel from '../models/note';
-import toNewNote from '../../utils/notes';
+import toNewNote from '../utils/notes';
 
 // GET all notes
 const getAllNotes =  (_req: Request, res: Response, next: NextFunction) => {
