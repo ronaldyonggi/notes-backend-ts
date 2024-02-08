185825cecde16d3466aecb617ff3a701f06d2e3f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-08 15:13:32 +0800

    toNewNote validation moved to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,42 @@
+import { NewNote } from '../types/note';
 // String type guard
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
 
-export {
-  isString,
+
+// String Parser
+const parseString = (str: unknown): string => {
+  if (!isString(str)) {
+    throw new Error(`Invalid input: ${str}`);
+  }
+  return str;
 };
+
+// Boolean parser
+const parseBoolean = (str: unknown): boolean => {
+  if (!(typeof str === 'boolean')) {
+    throw new Error(`Invalid input: ${str}`);
+  }
+  return str;
+};
+
+// toNewNote
+const toNewNote = (object: unknown): NewNote => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data');
+  }
+
+  if ('content' in object) {
+    const newNote = {
+      content: parseString(object.content),
+      important: 'important' in object ? parseBoolean(object.important) : false,
+    };
+
+    return newNote;
+  }
+
+  throw new Error('Invalid input data!');
+};
+
+export default toNewNote;
