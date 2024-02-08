050e463d332475f5027271c3cbf3b8d1919815f8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-08 15:12:47 +0800

    change Note type file name

--- src/models/note.ts
+++ src/models/note.ts
@@ -1,21 +1,21 @@
 import mongoose from 'mongoose';
-import { Note } from '../types/notes';
+import { Note } from '../types/note';
 
 const noteSchema = new mongoose.Schema<Note>({
   content: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
-  important: Boolean
+  important: Boolean,
 });
 
 noteSchema.set('toJSON', {
-  transform: (_document, returnedObject: Record<string,string>) => {
+  transform: (_document, returnedObject: Record<string, string>) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-  }
+  },
 });
 
 export default mongoose.model<Note>('Note', noteSchema);
