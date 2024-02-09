5233b1c48c74797430d3f64136281970b282ef30
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-09 14:36:04 +0800

    defined tests for reverse function

---
+++ src/tests/reverse.test.ts
@@ -0,0 +1,21 @@
+import {reverse} from './for_testing';
+
+test('reverse of a', () => {
+  const result = reverse('a');
+  expect(result).toBe('a');
+});
+
+test('reverse of react', () => {
+  const result = reverse('react');
+  expect(result).toBe('tcaer');
+});
+
+test('reverse of releveler', () => {
+  const result = reverse('releveler');
+  expect(result).toBe('releveler');
+});
+
+test('palindrome of react', () => {
+  const result = reverse('react');
+  expect(result).toBe('tkaer');
+});
