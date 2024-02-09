ddb55390e9c92ae9f8de3e7213e63881e96d21b9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-09 14:35:39 +0800

    defined dummy functions for testing purposes

---
+++ src/tests/for_testing.ts
@@ -0,0 +1,21 @@
+const reverse = (string: string) => {
+  return string
+    .split('')
+    .reverse()
+    .join('');
+};
+
+const average = (array: number[]) => {
+  const reducer = (sum: number, item: number) => {
+    return sum + item;
+  };
+
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length;
+};
+
+export {
+  reverse,
+  average
+};
