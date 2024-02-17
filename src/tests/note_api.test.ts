ee9ff5e148d6370b996839ffed94333368adf8a9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-17 12:37:08 +0800

    add test for testing delete functionality

--- src/tests/note_api.test.ts
+++ src/tests/note_api.test.ts
@@ -90,6 +90,22 @@ test('a specific note can be viewed', async () => {
   expect(resultNote.body).toEqual(noteToView);
 });
 
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map(r => r.content);
+  expect(contents).not.toContain(noteToDelete.content);
+});
+
 afterAll( async () => {
   await mongoose.connection.close();
 });
