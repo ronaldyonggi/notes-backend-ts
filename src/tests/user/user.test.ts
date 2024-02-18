330b2fc8629ae762c790cb53776816455539734f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-18 15:36:25 +0800

    create test that validates user creation with username that hasn't been used

--- src/tests/user/user.test.ts
+++ src/tests/user/user.test.ts
@@ -17,6 +17,30 @@ describe('when there is initially one user in db', () => {
     await user.save();
   });
 
+  test('creating a new user succeeds with a username that has not been used', async () => {
+    const usersAtStart = await helper.usersInDb();
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    // Check if the number of uses in DB is increased at the end
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    // Check if the newly created user's username exist
+    const usernames = usersAtEnd.map(user => user.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
