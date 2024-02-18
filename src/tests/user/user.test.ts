4c20b65dc408d9b089a6c33bd0e213fe9d29d662
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-18 15:44:14 +0800

    create test that validates new user creation with pre-existing username should fail

--- src/tests/user/user.test.ts
+++ src/tests/user/user.test.ts
@@ -39,6 +39,28 @@ describe('when there is initially one user in db', () => {
     const usernames = usersAtEnd.map(user => user.username);
     expect(usernames).toContain(newUser.username);
   });
+
+  test('creating a new user failed with proper status code and message if username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('expected `username` to be unique');
+
+    const usersAtEnd = await helper.usersInDb();
+    // Ensure number of users in DB is unchanged
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
 });
 
 afterAll(async () => {
