b187997e578dbec78a3550199d171595b49d9672
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-18 11:18:29 +0800

    defined mongo user model schema

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ]
+});
+
+// Modify how the object is returned when users are fetched
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // passwordHash should not be visible 
+    delete returnedObject.passwordHash;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
