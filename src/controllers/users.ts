4bb7c0f81595c15a3b2ebde6690ed881366debb0
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-18 12:42:53 +0800

    defined controller for user

---
+++ src/controllers/users.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import UserModel from '../models/user';
+import ts_utils from '../utils/ts_utils';
+
+// CREATE a new user
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { username, name, password } = ts_utils.toNewUser(req.body);
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const newUser = new UserModel({
+    username, name, passwordHash
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch(error) {
+    next(error);
+  }
+
+};
+
+export default {
+  createUser
+};
