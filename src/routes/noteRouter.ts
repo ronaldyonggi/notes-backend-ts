cdc8ed55aa2f161887226f29101a3c1a819fb5b1
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-19 14:05:52 +0800

    disable no-misused-promises ts eslint for entire file

--- src/routes/noteRouter.ts
+++ src/routes/noteRouter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
 import noteController from '../controllers/notes';
 const router = Router();
@@ -5,7 +6,6 @@ const router = Router();
 // Base router '/'
 router.route('/')
   .get(noteController.getAllNotes)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   .post(noteController.createNote);
 
 // Routes with id params
