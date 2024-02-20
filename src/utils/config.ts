281bcefe74fd758830ca5147c42b6d1459ed7132
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-20 13:00:04 +0800

    defined SECRET variable value from .env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,7 +6,10 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI;
 
+const SECRET = process.env.SECRET;
+
 export default {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
+  SECRET
 };
