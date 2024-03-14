7ed4d9f0fb88965067e7814b31a5f3fae130eadc
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-14 10:07:52 +0800

    define property user to express Request

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,11 +1,11 @@
 export {};
 
-// Tells TypeScript that req object has 'path' and 'token' property
 declare global {
   namespace Express {
     interface Request {
       path: string
       token: string
+      user
     }
   }
 }
